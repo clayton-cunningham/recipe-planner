9e5a0d2636f89fed56ab57be9fb0bce954be9313
Clayton Cunningham <clayton.c.cunnigham@gmail.com>
2024-10-13 13:58:40 -0400

    Refactor: moving logic into new component

---
+++ src/components/primary-widgets/selectors/PokemonSelector.tsx
@@ -0,0 +1,103 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { BoxEntry, IngredientLevel, ingredients, pokedex, TypeGroup } from "../../../assets/resources";
+import { Row } from "../../generic/Row";
+import { formatIdForPng } from "../helpers";
+import { AppContext } from "../../../App";
+import "./Selectors.less"
+import { Column } from "../../generic/Column";
+import { Pill } from "../../generic/Pill";
+import { HoverHighlight } from "../../generic/HoverHighlight";
+
+export const PokemonSelector = (props: {typeGroup: TypeGroup, context: AppContext, excludeLevel60: boolean}) => {
+
+    const {typeGroup, context, excludeLevel60} = props;
+    
+    var dexEntry = pokedex.find(p => p.name == typeGroup.default)!;
+    if (dexEntry == undefined) return null;
+
+    var monState = context.ownedPokemon.find(oP => oP.id == dexEntry.id);
+
+    const getIngredientPillState = (monState: BoxEntry | undefined) => {
+        // We can have deactivated entries with filled in data, so we have to check both fields for 30 & 60.
+        if (monState?.Perf && monState?.ingredientLevel == IngredientLevel.Lvl60) return "left-3";
+        if (monState?.Perf && monState?.ingredientLevel == IngredientLevel.Lvl30) return "left-2";
+        if (monState?.Perf) return "left-1";
+        else return "left-0"
+    }
+
+    const getExludePillState = (excludeLevel60: boolean) => {
+        if (excludeLevel60) return "right-1";
+        else return "right-0"
+    }
+
+    return (
+        <Column
+            key={typeGroup.key + "_berry_mon"} 
+            className={"pokemon-selector " + (monState?.Perf ? "can-use" : "cant-use")}
+        >
+            <div onClick={() => context.togglePokemon(dexEntry)}>
+                <div className="main-icon">
+                    <img
+                        src={dexEntry.portraitUri} 
+                        // src={formatIdForPng(p.id)} 
+                        className="img-m"
+                    />
+                    <Row className="sub-icon-group">
+                        {typeGroup.pokemon.filter(subP => subP != dexEntry.name).map(subP => {
+                            var subDexEntry = pokedex.find(p => p.name == subP)!;
+                            if (subDexEntry == undefined) return null;
+
+                            return (
+                                <img
+                                    key={subDexEntry.id + "_sub-dex-entry"}
+                                    className="img-xs"
+                                    src={subDexEntry.portraitUri}
+                                />
+                            )
+                        })}
+                    </Row>
+                </div>
+                <Row className="pokemon-ingredients">
+                    {dexEntry.ingredient_3 &&
+                        <Pill className={"grey " + getExludePillState(excludeLevel60)} />
+                    }
+                    <Pill className={"green " + getIngredientPillState(monState)} />
+                    <HoverHighlight className="img-xs">
+                        <img 
+                            src={ingredients.find(i => i.name == dexEntry.ingredient_1)?.uri}
+                            className="img-xs"
+                            onClick={(event) => {
+                                context.selectPokemonIngredients(dexEntry, IngredientLevel.Lvl0); 
+                                event.stopPropagation();
+                            }}
+                        />
+                    </HoverHighlight>
+                    <HoverHighlight className="img-xs">
+                        <img 
+                            src={ingredients.find(i => i.name == dexEntry.ingredient_2)?.uri}
+                            className="img-xs"
+                            onClick={(event) => {
+                                context.selectPokemonIngredients(dexEntry, IngredientLevel.Lvl30); 
+                                event.stopPropagation();
+                            }}
+                        />
+                    </HoverHighlight>
+                    {dexEntry.ingredient_3 ?
+                        <HoverHighlight className="img-xs">
+                            <img 
+                                src={ingredients.find(i => i.name == dexEntry.ingredient_3)?.uri}
+                                className="img-xs"
+                                onClick={(event) => {
+                                    context.selectPokemonIngredients(dexEntry, IngredientLevel.Lvl60); 
+                                    event.stopPropagation();
+                                }}
+                            />
+                        </HoverHighlight>
+                        :
+                        <div className="img-xs"/>
+                    }
+                </Row>
+            </div>
+        </Column>
+    )
+}
--- src/components/primary-widgets/selectors/TypeSelector.tsx
+++ src/components/primary-widgets/selectors/TypeSelector.tsx
@@ -8,6 +8,7 @@ import "./Selectors.less"
 import { Column } from "../../generic/Column";
 import { Pill } from "../../generic/Pill";
 import { HoverHighlight } from "../../generic/HoverHighlight";
+import { PokemonSelector } from "./PokemonSelector";
 
 export const TypeSelector = (props: {pokemon: Pokemon[], setPokemon: Dispatch<SetStateAction<Pokemon[]>>, context: AppContext, excludeLevel60: boolean}) => {
 
@@ -22,19 +23,6 @@ export const TypeSelector = (props: {pokemon: Pokemon[], setPokemon: Dispatch<Se
         setPokemon(pokemon);
         setActiveTypeGroups(typeGroupSubsets);
     }, [title])
-
-    const getIngredientPillState = (monState: BoxEntry | undefined) => {
-        // We can have deactivated entries with filled in data, so we have to check both fields for 30 & 60.
-        if (monState?.Perf && monState?.ingredientLevel == IngredientLevel.Lvl60) return "left-3";
-        if (monState?.Perf && monState?.ingredientLevel == IngredientLevel.Lvl30) return "left-2";
-        if (monState?.Perf) return "left-1";
-        else return "left-0"
-    }
-
-    const getExludePillState = (excludeLevel60: boolean) => {
-        if (excludeLevel60) return "right-1";
-        else return "right-0"
-    }
 
     return (
         <Row className="type-selector">
@@ -62,79 +50,12 @@ export const TypeSelector = (props: {pokemon: Pokemon[], setPokemon: Dispatch<Se
             />
             <Row>
                 {activeTypeGroups.map(tG => {
-                    var dexEntry = pokedex.find(p => p.name == tG.default)!;
-                    if (dexEntry == undefined) return null;
-
-                    var monState = context.ownedPokemon.find(oP => oP.id == dexEntry.id);
                     return (
-                        <Column
-                            key={tG.key + "_berry_mon"} 
-                            className={"pokemon-selector " + (monState?.Perf ? "can-use" : "cant-use")}
-                        >
-                            <div onClick={() => context.togglePokemon(dexEntry)}>
-                                <div className="main-icon">
-                                    <img
-                                        src={dexEntry.portraitUri} 
-                                        // src={formatIdForPng(p.id)} 
-                                        className="img-m"
-                                    />
-                                    <Row className="sub-icon-group">
-                                        {tG.pokemon.filter(subP => subP != dexEntry.name).map(subP => {
-                                            var subDexEntry = pokedex.find(p => p.name == subP)!;
-                                            if (subDexEntry == undefined) return null;
-
-                                            return (
-                                                <img
-                                                    key={subDexEntry.id + "_sub-dex-entry"}
-                                                    className="img-xs"
-                                                    src={subDexEntry.portraitUri}
-                                                />
-                                            )
-                                        })}
-                                    </Row>
-                                </div>
-                                <Row className="pokemon-ingredients">
-                                    {dexEntry.ingredient_3 &&
-                                        <Pill className={"grey " + getExludePillState(excludeLevel60)} />
-                                    }
-                                    <Pill className={"green " + getIngredientPillState(monState)} />
-                                    <HoverHighlight className="img-xs">
-                                        <img 
-                                            src={ingredients.find(i => i.name == dexEntry.ingredient_1)?.uri}
-                                            className="img-xs"
-                                            onClick={(event) => {
-                                                context.selectPokemonIngredients(dexEntry, IngredientLevel.Lvl0); 
-                                                event.stopPropagation();
-                                            }}
-                                        />
-                                    </HoverHighlight>
-                                    <HoverHighlight className="img-xs">
-                                        <img 
-                                            src={ingredients.find(i => i.name == dexEntry.ingredient_2)?.uri}
-                                            className="img-xs"
-                                            onClick={(event) => {
-                                                context.selectPokemonIngredients(dexEntry, IngredientLevel.Lvl30); 
-                                                event.stopPropagation();
-                                            }}
-                                        />
-                                    </HoverHighlight>
-                                    {dexEntry.ingredient_3 ?
-                                        <HoverHighlight className="img-xs">
-                                            <img 
-                                                src={ingredients.find(i => i.name == dexEntry.ingredient_3)?.uri}
-                                                className="img-xs"
-                                                onClick={(event) => {
-                                                    context.selectPokemonIngredients(dexEntry, IngredientLevel.Lvl60); 
-                                                    event.stopPropagation();
-                                                }}
-                                            />
-                                        </HoverHighlight>
-                                        :
-                                        <div className="img-xs"/>
-                                    }
-                                </Row>
-                            </div>
-                        </Column>
+                        <PokemonSelector
+                            context={context}
+                            typeGroup={tG}
+                            excludeLevel60={excludeLevel60}
+                        />
                     )
                 })}
             </Row>
